3acf46f63d8039a56a23b63fdb2b57bfde79b33c
sunilxd <sunilkumar.dope@gmail.com>
2023-05-21 13:38:53 +0530

    admin page, flight company

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # flight-devrev
 Round 2 flight booking application
+
+
+```sqlite
+
+CREATE TABLE IF NOT EXISTS "customers" (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  email TEXT NOT NULL,
+  password_hashed TEXT NOT NULL
+);
+
+
+CREATE TABLE admin (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT NOT NULL,
+  password_hashed TEXT NOT NULL
+);
+
+
+CREATE TABLE flight_company (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL
+);
+
+
+CREATE TABLE flights (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  company_id INTEGER NOT NULL,
+  departure_date DATE NOT NULL,
+  departure_time TIME NOT NULL,
+  origin TEXT NOT NULL,
+  destination TEXT NOT NULL, price DECIMAL(10,2) NOT NULL, seats_available INT NOT NULL, duration DECIMAL(10,2) NOT NULL,
+  FOREIGN KEY (company_id) REFERENCES flight_company (id)
+);
+
+
+CREATE TABLE bookings (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  customer_id INTEGER NOT NULL,
+  flight_id INTEGER NOT NULL,
+  number_of_tickets INTEGER NOT NULL,
+  status TEXT NOT NULL,
+  created_at DATETIME NOT NULL,
+  FOREIGN KEY (customer_id) REFERENCES customers(id),
+  FOREIGN KEY (flight_id) REFERENCES flights(id)
+);
+
+
+CREATE TRIGGER reduce_available_seats_and_confirm_booking
+AFTER INSERT ON bookings
+BEGIN
+    UPDATE flights
+    SET seats_available = seats_available - NEW.number_of_tickets
+    WHERE id = NEW.flight_id;
+
+    UPDATE bookings
+    SET status = 'Confirmed'
+    WHERE id = NEW.id;
+END;
+
+```
--- sql_cmds.py
+++ sql_cmds.py
@@ -1,11 +1,35 @@
 import sqlite3
+import hashlib
 
-conn = sqlite3.connect('database.db')
-cursor = conn.cursor()
+def hash_input(input_text):
+
+    hash_object = hashlib.sha256()
+    hash_object.update(input_text.encode("utf-8"))
+    hash_value = hash_object.hexdigest()
+
+    return hash_value
+
+
+# to avoid multiple thread error
+def with_cursor(func):
+
+    def wrapper(*args, **kwargs):
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+
+        response = func(conn, cursor, *args, **kwargs)
+
+        cursor.close()
+        conn.close()
+
+        return response
+    
+    return wrapper
 
 
 # login signup
-def register(email, password):
+@with_cursor
+def register(conn, cursor, email, password):
 
     cursor.execute("SELECT * FROM customer WHERE email = ?", (email,))
     
@@ -13,15 +37,16 @@ def register(email, password):
     if len(already) != 0:
         return {"status": "bad", "msg": "Email already registered"}
     
-    password = str(hash(password))
+    password = hash_input(password)
 
     cursor.execute("INSERT INTO customer (email, password_hashed) VALUES (?, ?)", (email, password,))
     conn.commit()
 
-    return {"status": "good", "msg": "Reistered!"}
+    return {"status": "good", "msg": "Registered!"}
 
 
-def login(email, password):
+@with_cursor
+def login(conn, cursor, email, password):
 
     cursor.execute("SELECT * FROM customer WHERE email = ?", (email,))
     
@@ -29,7 +54,7 @@ def login(email, password):
     if len(already) == 0:
         return {"status": "bad", "msg": "email not found"}
     
-    password = str(hash(password))
+    password = hash_input(password)
 
     if already[0][-1] != password:
         return {"status": "bad", "msg": "incorrect password"}
@@ -37,7 +62,8 @@ def login(email, password):
     return {"status": "good", "msg": "loggedin", "id":already[0][0]}
 
 
-def admin_register(username, password):
+@with_cursor
+def admin_register(conn, cursor, username, password):
 
     cursor.execute("SELECT * FROM admin WHERE username = ?", (username,))
     
@@ -45,14 +71,15 @@ def admin_register(username, password):
     if len(already) != 0:
         return {"status": "bad", "msg": "Username already registered"}
     
-    password = str(hash(password))
+    password = hash_input(password)
 
     cursor.execute("INSERT INTO admin (username, password_hashed) VALUES (?, ?)", (username, password,))
     conn.commit()
 
     return {"status": "good", "msg": "Reistered!"}
 
-def admin_login(username, password):
+@with_cursor
+def admin_login(conn, cursor, username, password):
 
     cursor.execute("SELECT * FROM admin WHERE username = ?", (username,))
     
@@ -60,7 +87,7 @@ def admin_login(username, password):
     if len(already) == 0:
         return {"status": "bad", "msg": "username not found"}
     
-    password = str(hash(password))
+    password = hash_input(password)
 
     if already[0][-1] != password:
         return {"status": "bad", "msg": "incorrect password"}
@@ -68,4 +95,26 @@ def admin_login(username, password):
     return {"status": "good", "msg": "loggedin", "id":already[0][0]}
 
 
-# flight add remove
+# add flight company
+@with_cursor
+def list_company(conn, cursor):
+
+    cursor.execute("SELECT * FROM flight_company")
+    return [flight[1] for flight in cursor.fetchall()]
+
+
+@with_cursor
+def add_company(conn, cursor, company_name):
+
+    cursor.execute("SELECT * FROM flight_company WHERE name = ?", (company_name, ))
+    
+    if len(cursor.fetchall()) != 0:
+        return {"status": "bad", "msg": f"{company_name} already added"}
+
+    cursor.execute("INSERT INTO flight_company (name) VALUES (?)", (company_name, ))
+    conn.commit()
+
+    return {"status": "good", "msg": f"{company_name} added"}
+
+
+# add flights
